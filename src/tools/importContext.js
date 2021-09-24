a9103232621ba8b1bac56fbf254cc6622b073ab6
MadGoby <stas.forostyuk@gmail.com>
2021-09-24 18:31:54 +0300

    Header and Footers for pages in templates.

--- src/tools/importContext.js
+++ src/tools/importContext.js
@@ -3,8 +3,8 @@ function importAll(context) {
 }
 
 function importContext() {
-  importAll(((require.context('./../common', true, /.scss$/))));
-  importAll(((require.context('./../blocks', true, /.scss$|Init.js/))));
+  importAll((require.context('./../common', true, /.scss$/)));
+  importAll((require.context('./../blocks', true, /.scss$|Init.js/)));
 }
 
 export { importContext }
