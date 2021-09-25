f889e1845ebdb1f6f484cb2440f110231ca93f44
MadGoby <stas.forostyuk@gmail.com>
2021-09-25 18:33:57 +0300

    "Best Practices" fixes

--- src/tools/importContext.js
+++ src/tools/importContext.js
@@ -1,10 +1,12 @@
 function importAll(context) {
+  context.keys().map(elem=>console.log(elem));
   context.keys().map(context);
 }
 
 function importContext() {
   importAll((require.context('./../common', true, /.scss$/)));
   importAll((require.context('./../blocks', true, /.scss$|Init.js/)));
+  importAll((require.context('./../favicons', true, /\.(svg|png)$/ )));
 }
 
 export { importContext }
