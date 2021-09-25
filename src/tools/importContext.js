74b6efb89a0732a7d9f5ecdde6ab06c394374794
MadGoby <stas.forostyuk@gmail.com>
2021-09-25 21:26:04 +0300

    Import fixes

--- src/tools/importContext.js
+++ src/tools/importContext.js
@@ -1,12 +1,11 @@
 function importAll(context) {
-  context.keys().map(elem=>console.log(elem));
   context.keys().map(context);
 }
 
 function importContext() {
   importAll((require.context('./../common', true, /.scss$/)));
   importAll((require.context('./../blocks', true, /.scss$|Init.js/)));
-  importAll((require.context('./../favicons', true, /\.(svg|png)$/ )));
+  importAll((require.context('./../favicons', true, /.png$/)));
 }
 
 export { importContext }
