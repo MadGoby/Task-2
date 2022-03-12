0e5d5a8021eef18af56f74f259ea1e8df4bd1c5f
MadGoby <stas.forostyuk@gmail.com>
2022-03-13 00:45:56 +0300

    #16 Pug fixes

--- src/blocks/list/List.js
+++ src/blocks/list/List.js
@@ -9,7 +9,7 @@ class ListDropdown {
 
   getHtmlElements() {
     this.list = this.container.querySelector('.js-list__container');
-    this.title = this.container.querySelector('.js-title');
+    this.title = this.container.querySelector('.js-list__title');
   }
 
   handleTitleClick() {
